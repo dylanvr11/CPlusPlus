5cb571352d2b99c34150fee722906ad3b7eb375d
dylanvr11 <dylanvr97@gmail.com>
2024-03-01 23:35:25 -0500

    Structs

---
+++ Structs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	struct Character{
+		int edad;
+		int tel;
+	}Santiago;
+	Santiago.edad = 20;
+	Santiago.tel = 31654575476;
+	cout << Santiago.edad << endl;
+	cin.get();
+	return 0;
+}
